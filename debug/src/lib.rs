2a04eed4765fcac2dad260a5b72e46dadaa83ed6
crustydev <ademola1145@gmail.com>
2023-06-08 22:12:01 +0100

    feat(debug): exercises 05 and 06

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,11 +1,12 @@
 use quote::ToTokens;
+use std::collections::HashSet;
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let mut parsed = syn::parse_macro_input!(input as syn::DeriveInput);
 
     let ident = &parsed.ident;
-    let fields = if let syn::Data::Struct(syn::DataStruct { fields, .. }) = parsed.data {
+    let fields = if let syn::Data::Struct(syn::DataStruct { ref fields, .. }) = parsed.data {
         fields
     } else {
         panic!("Expected struct");
@@ -50,9 +51,35 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         }
     });
 
-    parsed.generics.params.iter_mut().for_each(|param| {
-        if let syn::GenericParam::Type(syn::TypeParam { bounds, .. }) = param {
-            bounds.push(syn::parse_quote!(std::fmt::Debug));
+    parsed.generics.params.iter_mut().for_each(|ref mut param| {
+        let fields = if let syn::Data::Struct(syn::DataStruct { ref fields, .. }) = &parsed.data {
+            fields
+        } else {
+            panic!("expected struct fields");
+        };
+
+        if let syn::GenericParam::Type(syn::TypeParam {
+            ident: generic_ident,
+            bounds,
+            ..
+        }) = param
+        {
+            let mut bounds_set: HashSet<syn::TypeParamBound> = std::collections::HashSet::new();
+            for field in fields {
+                let path = if let syn::Type::Path(syn::TypePath { path, .. }) = &field.ty {
+                    path
+                } else {
+                    unimplemented!()
+                };
+
+                let segment = &path.segments[0];
+                if contains_inner_t(segment, generic_ident) {
+                    bounds_set.insert(syn::parse_quote!(std::fmt::Debug));
+                }
+            }
+            bounds.extend(bounds_set.into_iter())
+        } else {
+            unimplemented!();
         }
     });
 
@@ -69,4 +96,56 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     };
 
     proc_macro::TokenStream::from(res)
+}
+
+// Walks the type recursively and returns either when it finds some reference to T or the end.
+// Should identify that T contains T.
+// Should identify that Box<Vec<T>> contains T.
+// Should identify that NonNull<Vec<HashMap<K, A>>> does not contain T.
+// Should identify that Vec<Phantom<Box<Vec<T>>>> does not contain T.
+fn contains_inner_t(path_segment: &syn::PathSegment, t_ident: &syn::Ident) -> bool {
+    if &path_segment.ident == t_ident {
+        return true;
+    }
+
+    if path_segment.ident == "PhantomData" {
+        return false;
+    }
+
+    let res = match &path_segment.arguments {
+        syn::PathArguments::None => return false,
+        syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+            args, ..
+        }) => args.iter().any(|arg| {
+            if let syn::GenericArgument::Type(syn::Type::Path(syn::TypePath { path, .. })) = arg {
+                path.segments
+                    .iter()
+                    .any(|segment| contains_inner_t(segment, t_ident))
+            } else {
+                false
+            }
+        }),
+        _ => unimplemented!(),
+    };
+
+    res
+}
+
+#[allow(dead_code)]
+fn extract_inner_type(path_segment: &syn::PathSegment) -> Option<String> {
+    let args = if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+        args,
+        ..
+    }) = &path_segment.arguments
+    {
+        args
+    } else {
+        unimplemented!();
+    };
+
+    if let syn::GenericArgument::Type(syn::Type::Path(syn::TypePath { path, .. })) = &args[0] {
+        Some(path.segments[0].ident.to_string())
+    } else {
+        None
+    }
 }
